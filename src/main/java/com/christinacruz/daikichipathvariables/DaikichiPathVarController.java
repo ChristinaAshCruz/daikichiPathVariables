67280b84d02a0c7613bd9d875eadef1c1a198b93
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-02-26 21:48:19 -0800

    added travelMessage and lottoMessage

--- src/main/java/com/christinacruz/daikichipathvariables/DaikichiPathVarController.java
+++ src/main/java/com/christinacruz/daikichipathvariables/DaikichiPathVarController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/daikichi")
 public class DaikichiPathVarController {
-	@RequestMapping("")
-	public String index() {
-		return "Welcome!";
+	@RequestMapping("/{name}")
+	public String index(@PathVariable("name") String name) {
+		return "Welcome " + name + "!";
 	}
-	@RequestMapping("/today")
-	public String today() {
-		return "Today you will find luck in all your endeavors!";
+	@RequestMapping("/today/{objectFound}")
+	public String today(@PathVariable("objectFound") String objectFound) {
+		return "Today you will find " + objectFound + " in all your endeavors!";
 	}
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
@@ -25,4 +25,15 @@ public class DaikichiPathVarController {
 	public String travelMessage(@PathVariable("location") String travel) {
 		return "Congratulations! You will soon travel to " + travel + "!";
 	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lottoMessage(@PathVariable("number") Integer number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
+	
+	
 }
